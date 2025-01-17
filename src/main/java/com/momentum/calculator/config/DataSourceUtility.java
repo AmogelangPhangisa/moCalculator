2f66661d4ffce988c58852188ae3b008e204c3de
amogelang <amogelangphangi@gmail.com>
2025-01-18 01:32:00 +0200

    first commit

--- src/main/java/com/momentum/calculator/config/DataSourceUtility.java
+++ src/main/java/com/momentum/calculator/config/DataSourceUtility.java
@@ -16,38 +16,29 @@ import javax.sql.DataSource;
  * @author S2028873
  */
 public class DataSourceUtility {
-
-    public static DataSource getDatasource() {
+    public static DataSource getDatasourceLookup() {
         HikariDataSource dataSource = new HikariDataSource();
         dataSource.setInitializationFailTimeout(0);
         dataSource.setMaximumPoolSize(5);
-//        dataSource.setDataSourceClassName("com.microsoft.sqlserver.jdbc.SQLServerDataSource");
-//        dataSource.addDataSourceProperty("url", "jdbc:sqlserver://LPTAYA61:2010;databaseName=calculatorDB");
-//        dataSource.addDataSourceProperty("user", "amogelang");
-//        dataSource.addDataSourceProperty("password", "amogelang");
+        
+        // AWS RDS MySQL configuration
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setJdbcUrl("jdbc:mysql://database-1.czye84gu27l8.eu-north-1.rds.amazonaws.com:3306/momecalculatordb");
-        dataSource.addDataSourceProperty("user", "admin");
-        dataSource.addDataSourceProperty("password", "Amogelang");
+        dataSource.setUsername("admin");
+        dataSource.setPassword("Amogelang");
         
+        // Connection pool optimizations
         dataSource.addDataSourceProperty("cachePrepStmts", "true");
         dataSource.addDataSourceProperty("prepStmtCacheSize", "250");
         dataSource.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
         dataSource.addDataSourceProperty("useServerPrepStmts", "true");
         
-
         return dataSource;
     }
-
     
-    public static DataSource getDatasourceLookup() {
-        try {
-            InitialContext context = new InitialContext();
-            DataSource dataSource = (DataSource) context.lookup("java:/calculatorDS");
-            return dataSource;
-        } catch (NamingException ex) {
-            Logger.getLogger(DataSourceUtility.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
+    // You can remove this method if it's not used elsewhere
+    public static DataSource getDatasource() {
+        return getDatasourceLookup();
     }
 }
+
